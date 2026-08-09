b54a5ef898feaa7b161d054f1df4852d65ac4142
Ludba-Idah <awaisabdulhadi58@gmail.com>
2026-08-09 14:25:41 +0500

    Add files via upload

---
+++ phone_book_v2.py
@@ -0,0 +1,34 @@
+phone_book = {}
+command = 0
+
+while True:
+    command = input("command (1 search, 2 add, 3 quit): ")
+
+    if command == "3":
+        print("quitting...")
+        break
+
+    else:
+        if command == "1":
+            name = input("name: ")
+
+            if name in phone_book:
+
+                for number in phone_book[name]:
+                    print(number)
+            else:
+                print("no number")
+
+
+        elif command == "2":
+            name = input("name: ")
+            number = input("number: ")
+
+            if name not in phone_book:
+                phone_book[name] = []
+
+            phone_book[name].append(number)
+            print("ok!")
+
+        else:
+            continue
