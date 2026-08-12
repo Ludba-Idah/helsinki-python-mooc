3a74bf652b14f8256c2268754cbb7caed09a0824
Ludba-Idah <awaisabdulhadi58@gmail.com>
2026-08-12 14:42:29 +0500

    Delete negative_to_positive.py

--- negative_to_positive.py
+++
@@ -1,7 +0,0 @@
-pos_integer = int(input("Please type in a positive integer: "))
-neg_integer = -pos_integer
-for i in range(neg_integer, pos_integer + 1):
-    if i == 0:
-        continue
-    
-    print(i)
