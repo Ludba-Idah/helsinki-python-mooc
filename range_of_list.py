b05c239562d95a479b54dc734aafc77a70695f6c
Ludba-Idah <awaisabdulhadi58@gmail.com>
2026-08-12 14:39:38 +0500

    Delete range_of_list.py

--- range_of_list.py
+++
@@ -1,8 +0,0 @@
-def range_of_list(my_list):
-    smallest = min(my_list)
-    largest = max(my_list)
-    return largest - smallest
-
-if __name__ == "__main__":
-    my_list = [3, 6, -4]
-    print(range_of_list(my_list))
