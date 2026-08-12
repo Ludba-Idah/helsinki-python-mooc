2a28da7495635e8940e297d2ebd2517bd19eb64f
Ludba-Idah <awaisabdulhadi58@gmail.com>
2026-08-12 15:53:57 +0500

    Add files via upload

---
+++ Part 4/part04-36_no_shouting_allowed/src/no_shouting_allowed.py
@@ -0,0 +1,15 @@
+def no_shouting(my_list):
+
+    new_list = []
+
+    for string in my_list:
+        if string.isupper():
+            continue
+        else:
+            new_list.append(string)
+    return new_list
+
+if __name__ == "__main__":
+    my_list = ["ABC", "def", "UPPER", "ANOTHERUPPER", "lower", "another lower", "Capitalized"]
+    pruned_list = no_shouting(my_list)
+    print(pruned_list)
